a03038cf07149adc37da939839ff0820a05d2c06
satishPanigrahi <satish.panigrahi@gmail.com>
2016-03-16 09:18:53 -0300

    Update get_global_formula.js

--- assets/js/get_global_formula.js
+++ assets/js/get_global_formula.js
@@ -1,49 +1,52 @@
 var global = [];
 
-var canada = _.findWhere(exportsToCanada, {'id':'Value 2013'});
+var canada = _.findWhere(importsToCanada, {'id':'Value 2013'});
 canada.id = 'Canada';
 global.push(canada);
 
-var Mexico = _.findWhere(exportsToMexico, {'id':'Value 2013'});
+var Mexico = _.findWhere(importsToMexico, {'id':'Value 2013'});
 Mexico.id = 'Mexico';
 global.push(Mexico);
 
-var china = _.findWhere(exportsToChina, {'id':'Value 2013'});
+var china = _.findWhere(importsToChina, {'id':'Value 2013'});
 china.id = 'China';
 global.push(china);
 
-var Japan = _.findWhere(exportsToJapan, {'id':'Value 2013'});
+var Japan = _.findWhere(importsToJapan, {'id':'Value 2013'});
 Japan.id = 'Japan';
 global.push(Japan);
 
-var France = _.findWhere(exportsToFrance, {'id':'Value 2013'});
+var France = _.findWhere(importsToFrance, {'id':'Value 2013'});
 France.id = 'France';
 global.push(France);
 
-var SaudiArabia = _.findWhere(exportsToSaudiArabia, {'id':'Value 2013'});
+var SaudiArabia = _.findWhere(importsToSaudiArabia, {'id':'Value 2013'});
 SaudiArabia.id = 'SaudiArabia';
 global.push(SaudiArabia);
 
-var israel = _.findWhere(exportsToIsrael, {'id':'Value 2013'});
+var israel = _.findWhere(importsToIsrael, {'id':'Value 2013'});
 israel.id = 'Israel';
 global.push(israel);
 
-var Argentina = _.findWhere(exportsToArgentina, {'id':'Value 2013'});
+var Argentina = _.findWhere(importsToArgentina, {'id':'Value 2013'});
 Argentina.id = 'Argentina';
 global.push(Argentina);
 
-var Iraq = _.findWhere(exportsToIraq, {'id':'Value 2013'});
+var Iraq = _.findWhere(importsToIraq, {'id':'Value 2013'});
 Iraq.id = 'Iraq';
 global.push(Iraq);
 
-var Pakistan = _.findWhere(exportsToPakistan, {'id':'Value 2013'});
+var Pakistan = _.findWhere(importsToPakistan, {'id':'Value 2013'});
 Pakistan.id = 'Pakistan';
 global.push(Pakistan);
 
-var haiti = _.findWhere(exportsToHaiti, {'id':'Value 2013'});
+var haiti = _.findWhere(importsToHaiti, {'id':'Value 2013'});
 haiti.id = 'Haiti';
 global.push(haiti);
 
+/*
+// Not included
+// Reason: Some commodities are not present
 var cuba = _.findWhere(exportsToCuba, {'id':'Value 2013'});
 cuba.id = 'Cuba';
 global.push(cuba);
@@ -55,4 +58,5 @@ global.push(SierraLeone);
 var VaticanCity = _.findWhere(exportsToVaticanCity, {'id':'Value 2013'});
 VaticanCity.id = 'VaticanCity';
 global.push(VaticanCity);
+*/
 
